c70d98d5c657a40bf3ff21cc81e743fb71e9d924
hazel <68608437+hwajinkim@users.noreply.github.com>
2024-09-14 16:11:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner (System.in);
+        
+        char c = sc.nextLine().charAt(0);
+        int num = (int)c;
+        System.out.println(num);
+        
+    }
+}
